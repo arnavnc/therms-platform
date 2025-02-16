55c2142ffe871332a5a4d5797db9c62fd4d27a01
Arnav Chakravarthy <achakravarthy298@student.fuhsd.org>
2025-02-16 04:54:02 -0800

    new changes

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -101,6 +101,13 @@ export default function Navbar() {
                     <div className="absolute right-0 top-full mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
                       <div className="py-1">
                         <Link
+                          href="/terra"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          onClick={() => setIsDropdownOpen(false)}
+                        >
+                          Connect a Wearable
+                        </Link>
+                        <Link
                           href="/profile"
                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                           onClick={() => setIsDropdownOpen(false)}
